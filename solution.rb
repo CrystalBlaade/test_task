d1e8eefb73fd6bd014d59fe8b1481f849e9ffbf9
Mark Rybalko <crystalblaade@gmail.com>
2022-12-26 08:22:59 +0400

    fix: removed unneccessary line

--- solution.rb
+++ solution.rb
@@ -1,4 +1,4 @@
-string = ARGV.join[0] || '123456789012345678909999991234569999'
+string = ARGV[0] || '123456789012345678909999991234569999'
 
 result = string.each_char.each_with_object(Hash.new(0)) do |char, acc|
   acc[char] += 1
