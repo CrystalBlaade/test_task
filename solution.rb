16f0b09b3596efbd8a1b874a1c3f3f34b8c8980a
Mark Rybalko <crystalblaade@gmail.com>
2022-12-26 08:10:31 +0400

    finished histogram task

---
+++ solution.rb
@@ -0,0 +1,9 @@
+string = ARGV.join[0] || '123456789012345678909999991234569999'
+
+result = string.each_char.each_with_object(Hash.new(0)) do |char, acc|
+  acc[char] += 1
+end
+
+result.each do |k, v|
+  puts "#{k}: #{'*' * v}"
+end
